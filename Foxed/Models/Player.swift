773fcff0f514c724c0076ce3ca7061e6a73e591b
Owen Campbell <ocampbel@sfu.ca>
2019-01-26 16:49:30 -0800

    initial

---
+++ Foxed/Models/Player.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Foxed
+//
+//  Created by Owen Campbell on 2019-01-26.
+//  Copyright © 2019 Owen Campbell. All rights reserved.
+//
+
+import Foundation
+
+struct Player
+{
+    var name: String;
+    var score: Int;
+}
